9705b535dac96fd07601db00e21c8c008a43ec78
acar12 <none>
2022-09-03 16:30:59 -0500

    basic window

---
+++ beach_game/constants.py
@@ -0,0 +1,4 @@
+import enum
+
+class Color(enum.Enum):
+    BLACK = (0, 0, 0)
---
+++ beach_game/game.py
@@ -0,0 +1,12 @@
+import pygame
+from constants import Color
+
+width, height = 800, 800
+display = pygame.display.set_mode((width, height))
+
+def draw():
+    pass
+
+running = True
+while running:
+    pygame.draw.rect(display, )
